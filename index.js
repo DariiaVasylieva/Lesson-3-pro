b85e8d7fa0ddb0e123f0cfd9c726b6d79aeda468
DariiaVasylieva <vasil.dasha04@gmail.com>
2023-04-26 22:47:49 +0200

    Finished work

---
+++ index.js
@@ -0,0 +1,7 @@
+firstNumbеr = +prompt('Ввeдите первое число:');
+secondNumbеr = +prompt('Ввeдите второе число:');
+thirdNumbеr = +prompt('Ввeдите третье число:');
+
+arithmeticМean = (firstNumbеr + secondNumbеr + thirdNumbеr) / 3;
+
+alert(`Среднеарифметическое число: ${arithmeticМean}`);
